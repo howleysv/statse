c994ee63a8146bca6c4c42b13b00c7e4e2b2a581
Shane Howley <howleysv@gmail.com>
2013-09-13 14:38:40 +0100

    Added public application interface. Added stat sender. Added stat prefix.

--- src/statse.erl
+++ src/statse.erl
@@ -2,4 +2,55 @@
 -author( "Shane Howley <howleysv@gmail.com>" ).
 
 %% Public interface
--export( [ ] ).
+-export( [ increment/1, increment/2, decrement/1, decrement/2, count/2, count/3, timing/2, gauge/2, gauge_change/2 ] ).
+
+-export_type( [ stat_key/0 ] ).
+
+-type stat_key() :: iodata().
+
+-spec increment( stat_key() ) -> ok.
+increment( Stat ) ->
+	count( Stat, 1 ).
+
+-spec increment( stat_key(), number() ) -> ok.
+increment( Stat, SampleRate ) ->
+	count( Stat, 1, SampleRate ).
+
+-spec decrement( stat_key() ) -> ok.
+decrement( Stat ) ->
+	count( Stat, -1 ).
+
+-spec decrement( stat_key(), number() ) -> ok.
+decrement( Stat, SampleRate ) ->
+	count( Stat, -1, SampleRate ).
+
+-spec count( stat_key(), integer() ) -> ok.
+count( Stat, Count ) ->
+	count( Stat, Count, 1.0 ).
+
+-spec count( stat_key(), integer(), number() ) -> ok.
+count( Stat, Count, SampleRate ) when SampleRate >= 1.0 ->
+	dispatch( { counter, Stat, Count } );
+
+count( Stat, Count, SampleRate ) ->
+	dispatch( { counter, Stat, Count, SampleRate } ).
+
+-spec timing( stat_key(), number() | erlang:timestamp() ) -> ok.
+timing( Stat, Timestamp = { _, _, _ } ) ->
+	Millis = timer:now_diff( erlang:now(), Timestamp ) / 1000,
+	timing( Stat, Millis );
+
+timing( Stat, Millis ) ->
+	dispatch( { timer, Stat, Millis } ).
+
+-spec gauge( stat_key(), number() ) -> ok.
+gauge( Stat, Value ) ->
+	dispatch( { gauge, Stat, Value } ).
+
+-spec gauge_change( stat_key(), number() ) -> ok.
+gauge_change( Stat, Delta ) ->
+	dispatch( { gauge_change, Stat, Delta } ).
+
+-spec dispatch( tuple() ) -> ok.
+dispatch( Message ) ->
+	statse_worker:send_stat( statse_worker, Message ).
--- src/statse_worker.erl
+++ src/statse_worker.erl
@@ -3,21 +3,26 @@
 -behaviour( gen_server ).
 
 %% Public interface
--export( [ start_link/0, start_link/2 ] ).
+-export( [ start_link/0, start_link/3, send_stat/2 ] ).
 
 %% gen_server behavour
 -export( [ init/1, handle_call/3, handle_cast/2, handle_info/2, terminate/2, code_change/3 ] ).
 
 %% Internal State
 -record( state, {
-			socket 	:: gen_udp:socket(),	%% The socket from which data is sent
-			ip 	:: inet:ip_address(),	%% IP of the statsd server
-			port 	:: inet:port_number()	%% Port number of the statsd server
+			socket 		:: gen_udp:socket(),	%% The socket from which data is sent
+			ip 		:: inet:ip_address(),	%% IP of the statsd server
+			port 		:: inet:port_number(),	%% Port number of the statsd server
+			stat_prefix	:: statse:stat_key()	%% Prefix namespace for all stats
 		} ).
 
 -type call_type() 	:: stop.
--type cast_type()	:: stop.
--type info_type()	:: stop.
+-type cast_type()	:: { counter, statse:stat_key(), integer() }
+			|  { counter, statse:stat_key(), integer(), float() }
+			|  { timer, statse:stat_key(), number() }
+			|  { gauge, statse:stat_key(), number() }
+			|  { gauge_change, statse:stat_key(), number() }.
+-type info_type()	:: _.
 -type call_ret() 	:: { noreply, #state{} }
 			|  { reply, term(), #state{} }
 			|  { stop, term(), term(), #state{} }.
@@ -28,17 +33,22 @@
 start_link() ->
 	{ ok, Host } = application:get_env( statsd_host ),
 	{ ok, Port } = application:get_env( statsd_port ),
-	start_link( Host, Port ).
+	{ ok, StatPrefix } = application:get_env( stat_prefix ),
+	start_link( Host, Port, StatPrefix ).
 
--spec start_link( inet:hostname(), inet:port_number() ) -> { ok, pid() }.
-start_link( Host, Port ) ->
-	gen_server:start_link( ?MODULE, { Host, Port }, [] ).
+-spec start_link( inet:hostname(), inet:port_number(), statse:stat_key() ) -> { ok, pid() }.
+start_link( Host, Port, StatPrefix ) ->
+	gen_server:start_link( ?MODULE, { Host, Port, StatPrefix }, [] ).
 
--spec init( { inet:hostname(), inet:port_number() } ) -> { ok, #state{} }.
-init( { Host, Port } ) ->
+-spec send_stat( atom() | pid(), tuple() ) -> ok.
+send_stat( StatClient, Stat ) ->
+	gen_server:cast( StatClient, Stat ).
+
+-spec init( { inet:hostname(), inet:port_number(), statse:stat_key() } ) -> { ok, #state{} }.
+init( { Host, Port, StatPrefix } ) ->
 	{ ok, IP } = inet:getaddr( Host, inet ),
 	{ ok, Socket } = gen_udp:open( 0, [ { active, false } ] ),
-	{ ok, #state{ socket = Socket, ip = IP, port = Port } }.
+	{ ok, #state{ socket = Socket, ip = IP, port = Port, stat_prefix = build_prefix( StatPrefix ) } }.
 
 -spec handle_call( call_type(), { pid(), term() }, #state{} ) -> call_ret().
 handle_call( stop, _From, #state{} = State ) ->
@@ -50,6 +60,36 @@ handle_call( _Request, _From, State ) ->
 	{ noreply, State }.
 
 -spec handle_cast( cast_type(), #state{} ) -> cast_ret().
+handle_cast( { counter, Stat, Count }, #state{} = State ) ->
+	Format = io_lib:format( "~p|c", [ Count ] ),
+	send_stat( Stat, Format, State ),
+	{ noreply, State };
+
+handle_cast( { counter, Stat, Count, SampleRate }, #state{} = State ) ->
+	Format = io_lib:format( "~p|c|@~p", [ Count, SampleRate ] ),
+	send_stat( Stat, Format, State ),
+	{ noreply, State };
+
+handle_cast( { timer, Stat, Millis }, #state{} = State ) ->
+	Format = io_lib:format( "~p|ms", [ Millis ] ),
+	send_stat( Stat, Format, State ),
+	{ noreply, State };
+
+handle_cast( { gauge, Stat, Value }, #state{} = State ) ->
+	Format = io_lib:format( "~p|g", [ Value ] ),
+	send_stat( Stat, Format, State ),
+	{ noreply, State };
+
+handle_cast( { gauge_change, Stat, Delta }, #state{} = State ) when Delta >= 0 ->
+	Format = io_lib:format( "+~p|g", [ Delta ] ),
+	send_stat( Stat, Format, State ),
+	{ noreply, State };
+
+handle_cast( { gauge_change, Stat, Delta }, #state{} = State ) ->
+	Format = io_lib:format( "-~p|g", [ Delta ] ),
+	send_stat( Stat, Format, State ),
+	{ noreply, State };
+
 %% Unrecognised request
 handle_cast( _Request, State ) ->
 	error_logger:warning_msg( "Unrecognized cast: ~p", [ _Request ] ),
@@ -68,3 +108,18 @@ terminate( _Reason, #state{ socket = Socket } ) ->
 -spec code_change( term(), #state{}, term() ) -> { ok, #state{} }.
 code_change( _OldVersion, State, _Extra ) ->
 	{ ok, State }.
+
+-spec build_prefix( statse:stat_key() ) -> statse:stats_key().
+build_prefix( [] ) -> [];
+
+build_prefix( Prefix ) ->
+	Binary = iolist_to_binary( Prefix ),
+	case binary:last( Binary ) of
+		$. ->	Binary;
+		_ ->	<<Binary/binary, $./integer>>
+	end.
+
+-spec send_stat( statse:stat_key(), iolist(), #state{} ) -> ok.
+send_stat( Stat, Data, #state{ socket = Socket, ip = IP, port = Port, stat_prefix = StatPrefix } ) ->
+	Packet = iolist_to_binary( [ StatPrefix, Stat, $:, Data ] ),
+	ok = gen_udp:send( Socket, IP, Port, Packet ).
